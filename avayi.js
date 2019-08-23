753c6a7955f9b276db5defed4ed745d09be8f543
Arnavjain1703 <arnavjainkht@gmail.com>
2019-08-24 02:11:10 +0530

    updated one

--- avayi.js
+++ avayi.js
@@ -1,33 +1,69 @@
 
-var el = document.getElementById("player");
-			
+var car = document.getElementById("player");
+
+var e = document.querySelectorAll(".up");
+var f = document.querySelectorAll(".down");
+
+
+
+// code for upper movement of blocks
+
 
 	window.addEventListener("keydown",checkKeyPress);
 		function checkKeyPress(key)
 		{
+
 			if(key.keyCode=="39")
 				{
-					el.style.left = (el.offsetLeft += 10) + 'px';
+					car.style.left = (car.offsetLeft += 5) + 'px';
 				}
 				if(key.keyCode=="37")
 
 				{
-					el.style.left = (el.offsetLeft -= 10) + 'px';
+					car.style.left = (car.offsetLeft -= 5) + 'px';
 				}	
 			if(key.keyCode=="40")
 				{
-					 el.style.top = (el.offsetTop += 10) + 'px';
+					 car.style.top = (car.offsetTop += 5) + 'px';
 				}
 			if(key.keyCode=="38")
 				{
-					el.style.top = (el.offsetTop -= 10) + 'px';
+					car.style.top = (car.offsetTop -= 5) + 'px';
 				}		
+         
+                 
+    
 		}
-			
-var object=document.querySelectorAll(".up")
-     
-   if(object[0].offsetLeft == el.offsetLeft)
-   {
-   	alert("navkarmantra");
-   }
+            
+var s = 1;            
+setInterval(function(){
+	for(var i=0;i<e.length;i++){
+    var eLeftPos = e[i].offsetTop;
+    var d=(eLeftPos + s)
+    e[i].style.top =  d  + 'px';
+    if(d>600)
+    {
+    	e[i].style.top= "0px";
+    }
+     // if(e.[i].offsetTop==car.offsetTop)
+     //             {
+     //             	alert("bhai tuch ho gaya");
+     //             }
+                    
+}
+}, 10);
+
+//code foe lower movement of blocks
+
+setInterval(function(){
+	for(var i=0;i<f.length;i++){
+    var posi = f[i].offsetTop;
+    var d=(posi - s)
+    f[i].style.top =  d  + 'px';
+    if(d<0)
+    {
+    	f[i].style.top= "600px";
+    }
+}
+}, 10);
 
